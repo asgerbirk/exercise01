c767b607617744de158907df5b45a91ca60f7593
asgerbirk <asger.birk@gmail.com>
2023-02-09 11:19:42 +0100

    commit in terminal

--- 00._Miscellaneous/1._Intro_Exercises/3.Objects.js
+++ 00._Miscellaneous/1._Intro_Exercises/3.Objects.js
@@ -1,33 +1,48 @@
-// --------------------------------------
+
 // Objects
 // --------------------------------------
 // Exercise 1 - Retrieve value from object by key
 
 const myObj = { message: "Hello, earthling! I bring peace." };
 
-// Log the message 
+// Log the message
+console.log(myObj.message);
 
 // --------------------------------------
-// Exercise 2 - Defining an object. 
+// Exercise 2 - Defining an object.
 
-// Create an object that has your name and age. 
+// Create an object that has your name and age.
+
+const me =
+    {
+        name: Asger,
+        age: 24
+    }
+console.log(me)
 
 
 // --------------------------------------
-// Exercise 3 - Add a property 
+// Exercise 3 - Add a property
 
 const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
+stackOverflow.isAllowed = true;
+console.log(stackOverflow)
 
 // --------------------------------------
-// Exercise 4 - Remove a property 
+// Exercise 4 - Remove a property
 
 const thisSong = { description: "The best song in the world." };
 
-// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+// remove the property "description" and add a property called "about" that should say "Just a tribute."
+
+delete thisSong.description;
+thisSong.about = "Just a tribute"
+console.log(thisSong)
 
 
 // --------------------------------------
+
 
 
